8d361a739598342dee47c1320a27b3c2d9a593f6
ArcGemu <67103350+ArcGemu@users.noreply.github.com>
2020-08-25 23:33:40 +1000

    Update lexer.go

--- celscript/lexer.go
+++ celscript/lexer.go
@@ -1,10 +1,18 @@
 package celscript
 
+import "fmt"
+
 type lexer struct {
 	In chan string
 }
 func NewLexer(in chan string) *lexer {
 	return &lexer{
 		In: in,
+	}
+}
+func (lexer *Lexer) Run() {
+	for {
+		msg := <- lexer.In
+		fmt.PrintIn(msg)
 	}
 }
