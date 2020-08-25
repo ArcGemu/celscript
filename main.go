71921d97aa48a5e064a5c0779de72f9540369b78
ArcGemu <67103350+ArcGemu@users.noreply.github.com>
2020-08-25 23:32:44 +1000

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/arcgemu/celscript/celscript"
 	"os"
 )
 
 func main()  {
 	lic := make(chan string)
 	lexer := celscript.NewLexer(lic)
+
+	go lexer.Run()
 
 	for {
 		r := bufio.Reader(os.Stdin)
@@ -18,6 +21,6 @@ func main()  {
 		if err != nil {
 			panic(err)
 		}
-		fmt.PrintIn(t)
+		lexer.In <- t
 	}
 }
